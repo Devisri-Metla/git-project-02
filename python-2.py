6e08290e36cc58ae8163b6922bd57b161f288bf7
devisri <devisri.metla@gmail.com>
2024-11-12 11:57:51 +0530

    list-programs

---
+++ python-2.py
@@ -0,0 +1,10 @@
+SECOND LARGEST ELEMENT IN LIST
+
+def second_largest(lst):
+    unique_lst = list(set(lst))  # Remove duplicates
+    unique_lst.sort()
+    return unique_lst[-2]
+
+# Example usage
+print(second_largest([1, 2, 3, 4, 5]))  # Output: 4
+
