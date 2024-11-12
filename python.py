f52be05d330ef21c4b13e459e044d9aaff46d69e
devisri <devisri.metla@gmail.com>
2024-11-12 11:28:04 +0530

    add file

---
+++ python.py
@@ -0,0 +1,9 @@
+def reverse_string(input_string):
+    return input_string[::-1]
+
+# Example usage
+original = "Hello, World!"
+reversed_str = reverse_string(original)
+print("Original String:", original)
+print("Reversed String:", reversed_str)
+
