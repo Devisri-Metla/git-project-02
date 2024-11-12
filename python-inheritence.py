54255ca00b7cc4f37339d9b0984477e94eb37f79
Devisri-Metla <114816512+Devisri-Metla@users.noreply.github.com>
2024-11-12 11:40:32 +0530

    Update python-inheritence.py

--- python-inheritence.py
+++ python-inheritence.py
@@ -22,5 +22,31 @@ print(f"{generic_animal.name} says: {generic_animal.speak()}")
 
 # Create an object of the derived class
 dog = Dog("Buddy", "Golden Retriever")
+
+
+-------------------------------------------------
+
+#Hierarchical Inheritance:
+class Parent:
+    def show(self):
+        print("This is the Parent class.")
+
+class Child1(Parent):
+    def display_child1(self):
+        print("This is Child1 class.")
+
+class Child2(Parent):
+    def display_child2(self):
+        print("This is Child2 class.")
+
+# Example usage
+child1 = Child1()
+child1.show()
+child1.display_child1()
+
+child2 = Child2()
+child2.show()
+child2.display_child2()
+
 print(f"{dog.name}, the {dog.breed}, says: {dog.speak()}")
 
