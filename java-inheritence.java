7b14f36cdacc32bb3454c6a69e55c31805eea660
devisri <devisri.metla@gmail.com>
2024-11-12 11:54:06 +0530

    modified file

--- java-inheritence.java
+++ java-inheritence.java
@@ -20,4 +20,37 @@ public class SingleInheritance {
         child.displayChild();
     }
 }
+---------------------------------------------------------------------
+MULTILEVEL INHERITENCE
+
+// Grandparent class
+class Grandparent {
+    void displayGrandparent() {
+        System.out.println("This is the Grandparent class.");
+    }
+}
+
+// Parent class
+class Parent extends Grandparent {
+    void displayParent() {
+        System.out.println("This is the Parent class.");
+    }
+}
+
+// Child class
+class Child extends Parent {
+    void displayChild() {
+        System.out.println("This is the Child class.");
+    }
+}
+
+public class MultilevelInheritance {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.displayGrandparent();
+        child.displayParent();
+        child.displayChild();
+    }
+}
+
 
