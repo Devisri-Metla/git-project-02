4612711ecd9ab74133b3095d95cc169ee99c444e
devisri <devisri.metla@gmail.com>
2024-11-12 19:35:04 +0530

    java fibbonacci

---
+++ java2.java
@@ -0,0 +1,18 @@
+JAVA FIBONACCI PROGRAM
+public class Fibonacci {
+    public static void fibonacci(int n) {
+        int a = 0, b = 1;
+        System.out.print(a + " " + b + " ");
+        for (int i = 2; i < n; i++) {
+            int c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+
+    public static void main(String[] args) {
+        fibonacci(10); // Output: 0 1 1 2 3 5 8 13 21 34
+    }
+}
+
